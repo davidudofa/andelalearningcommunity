2f90eaf1dd5fe8430f18fa26c7c11ddbef68f38f
Udofa Nnyene-ime <UdofaAlien@OutDoorTourss-MacBook-Pro.local>
2017-10-27 16:24:48 +0100

    fourth Commit

--- server.js
+++ server.js
@@ -14,28 +14,48 @@ app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: false }));
 
 // Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '/dist')));
 
 // Set our api routes
 app.use('/api', api);
 
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
+
 // Catch all other routes and return the index file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
+app.listen(process.env.PORT || 8080);
 /**
  * Get port from environment and store in Express.
  */
-const port = process.env.PORT || '3039';
-app.set('port', port);
+//const port = process.env.PORT || '8080';
+//app.set('port', port);
 
 /**
  * Create HTTP server.
  */
-const server = http.createServer(app);
+//const server = http.createServer(app);
 
 /**
  * Listen on provided port, on all network interfaces.
  */
-server.listen(port, () => console.log(`API running on localhost:${port}`));
+//server.listen(port, () => console.log(`API running on localhost:${port}`));
