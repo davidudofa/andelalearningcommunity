c394b8d18c5faeb628aeae9fcd0135d948819909
Udofa Nnyene-ime <nyime2@gmail.com>
2017-10-28 15:46:23 +0100

    final commit

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.get('*', (req, res) => {
 /**
  * Get port from environment and store in Express.
  */
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8083;
 app.set('port', port);
 
 /**
